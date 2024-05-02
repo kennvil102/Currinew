a9d380342ec43f9759d7dd9cf55ec83d2b6e17c5
kennvil102 <kennvilchez@gmail.com>
2024-05-02 10:22:21 -0600

    actualizado

---
+++ funciones.js
@@ -0,0 +1,36 @@
+let btn1 = document.getElementById("btn1").onclick = function() {
+    boton1()
+}
+function boton1() {
+    alert("Me gustan las peliculas")
+    
+}
+let btn2 = document.getElementById("btn2").onclick = function() {
+    boton2()
+}
+function boton2() {
+    alert("Los viajes")
+    
+}
+let btn3 = document.getElementById("btn3").onclick = function() {
+    boton3()
+}
+function boton3() {
+    alert("Leer mas o menos")
+    
+}
+let btn4 = document.getElementById("btn4").onclick = function() {
+    boton4()
+}
+function boton4() {
+    alert("Hala madrid")
+    
+}
+let btn5 = document.getElementById("btn5").onclick = function() {
+    boton5()
+}
+function boton5() {
+    alert("Musica")
+    
+}
+
